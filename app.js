5035f151d68b0e48e4530b9e9924df674a568841
Huy <h.qt193@gmail.com>
2022-12-30 18:14:19 -0500

    render basketball and add animation

--- app.js
+++ app.js
@@ -4,11 +4,39 @@ const ctx = canvas.getContext('2d')
 const textArray = ['a','s','f','e','g','y','j','y','m','o','p']
 const random = Math.floor(Math.random() * textArray.length)
 const randomText = textArray[random]
-    
-    
-    
 
 
-ctx.font = '20px serif'
-const text = ctx.fillText(`${randomText}`, 20, 20)
+const Basketball = {
+    x: 50,
+    y: 100,
+    rad: 10,
+    color: 'orange',
+    render() {
+        ctx.beginPath()
+        ctx.fillStyle = this.color
+        ctx.arc(this.x, this.y, this.rad, 0, Math.PI * 2)
+        ctx.fill()
+    }
+}
 
+const animation = () => {
+    ctx.clearRect(Basketball.x - 50, Basketball.y - 100, canvas.width, canvas.height)
+    Basketball.render()
+    if(Basketball.x < canvas.width) {
+        Basketball.x += 2
+        //console.log(Basketball.x)
+    }
+    if(Basketball.y > 50) {
+        Basketball.y += -1
+        console.log(Basketball.y)
+    }
+}
+
+// ctx.font = '20px serif'
+// const text = ctx.fillText(`${randomText}`, 20, 20)
+
+const gameInterval = setInterval(animation, 20)
+
+document.addEventListener('DOMContentLoaded',()=>{
+    gameInterval
+})
